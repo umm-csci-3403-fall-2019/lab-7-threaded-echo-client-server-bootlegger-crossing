0e55712902d3f4eda29d17af2a6fd2122c9ddd95
deglm006 <deglm006@morris.umn.edu>
2019-10-31 10:55:34 -0500

    Refactor and finish implementation
    Echoserver now contains the correspondent pool, as opposed to a separate
    class.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -5,7 +5,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import echoserver.ServerCorrespondentPool;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class EchoServer {
 	public static final int PORT_NUMBER = 6013;
@@ -17,8 +18,26 @@ public class EchoServer {
 
 	private void start() throws IOException, InterruptedException {
 
-		ServerCorrespondentPool correspondentPool = new ServerCorrespondentPool(PORT_NUMBER);
-		correspondentPool.start();
+		//the ServerSocket we are listening to
+		final ServerSocket mainSocket;
+
+		//our actual threadpool
+		final ExecutorService pool;
+
+		//setup main socket
+		mainSocket = new ServerSocket(this.PORT_NUMBER);
+
+		//initialize pool
+		pool = Executors.newFixedThreadPool(8);
+
+		try {
+			while(true){
+				pool.execute(new ServerCorrespondent(mainSocket.accept()));
+			}
+		} catch (IOException ex){
+			System.err.println(ex);
+			pool.shutdown();
+		}
 		}
 	}
 
--- src/echoserver/ServerCorrespondent.java
+++ src/echoserver/ServerCorrespondent.java
@@ -3,29 +3,47 @@ package echoserver;
 //This class handles the details of echoing an individual client
 //it implements runnable, and should be used from ServerCorrespondentPool
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 class ServerCorrespondent implements Runnable{
   private final Socket socket;
 
   //constructor
-  ServerCorrespondent (Socket socket){
+  public ServerCorrespondent (Socket socket){
     this.socket = socket;
   }
 
-  public void run(){
-    System.out.println("Connection established");
-    InputStream input = socket.getInputStream();
-    OutputStream output = socket.getOutputStream();
 
-    //we now have a connected client
-    //Read its first byte
-    Integer input_byte = input.read();
-    System.out.println(input_byte);
-    //if the present byte isn't null, continue reading and writing
-    while (input_byte != -1){
-      output.write(input_byte);
-      output.flush();
-      input_byte = input.read();
+  public void run() {
+    System.out.println("Connection established");
+    InputStream input = null;
+    OutputStream output = null;
+    boolean cont = true;
+
+    try {
+      input = socket.getInputStream();
+      output = socket.getOutputStream();
+      //we now have a connected client
+      //Read its first byte
+      Integer input_byte = input.read();
       System.out.println(input_byte);
+
+      //if the present byte isn't null, continue reading and writing
+      while (cont){
+        output.write(input_byte);
+        output.flush();
+        input_byte = input.read();
+        System.out.println(input_byte);
+
+        if( input_byte == -1){
+          cont = false;
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
     System.out.println("Connection closed");
   }
